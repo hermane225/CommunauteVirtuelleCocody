dc578733df3a009ff280bfc03f1d251c522e7ecc
hermane225 <mj2190175@gmail.com>
2025-10-19 01:09:04 +0000

    add  new component testPage

--- cvcocody/src/router/index.js
+++ cvcocody/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import pageAccuiel from '@/components/pageAccuiel.vue'
+import testPage from '@/components/testPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'pageaccuiel',
       component: pageAccuiel,
+    },
+    {
+      path: '/testPage',
+      name: 'testPage',
+      component: testPage,
     },
     {
       path: '/about',
