afda67e9cd71b5ec5029b4c62f201cadd9f0ddb5
hermane225 <mj2190175@gmail.com>
2025-10-22 12:37:45 +0000

    update bureauPage

--- cvcocody/src/components/bureauPage.vue
+++ cvcocody/src/components/bureauPage.vue
@@ -5,6 +5,14 @@
         <h2 class="text-3xl font-bold text-gray-800 mb-8 text-center">
           Les Membres du bureau de la Communauté Virtuelle de Cocody
         </h2>
+        <!-- <br/> -->
+        <p class="text-2xl mx-auto px-4">
+          Notre bureau est composé d’étudiants dynamiques et engagés, élus pour représenter et
+          coordonner les activités de la communauté. Ensemble, ils œuvrent au bon fonctionnement de
+          la CVC, à la mise en place d’initiatives locales, et à la promotion des valeurs de
+          l’Université Virtuelle de Côte d’Ivoire à Cocody.
+        </p>
+        <br />
         <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
           <!-- <div class="bg-white rounded-lg shadow-md overflow-hidden">
           <img
@@ -37,33 +45,13 @@
           </div>
         </div> -->
           <div class="bg-white rounded-lg shadow-md overflow-hidden">
-            <img src="/src/assets/noufe.jpg" alt="affiche" class="w-full h-75 object-cover" />
+            <img src="/src/assets/gboho.jpg" alt="affiche" class="w-full h-75 object-cover" />
             <div class="p-6 text-center">
               <h3 class="text-xl font-medium text-gray-800 mb-2">
                 <u> GHOHO NOUFE</u>
               </h3>
               <div class="text-gray-700 text-base">
                 Président de la Communauté Virtuelle de Cocody
-                <!-- <details>
-                  <summary>Voir Plus</summary>
-                  <p>
-                    L’événement a offert à chacun l’occasion de faire connaissance, partager ses
-                    idées et découvrir les multiples opportunités offertes par la communauté. La
-                    journée a débuté par une séance d’accueil et d’introduction, présentant la
-                    vision et les objectifs de la communauté ainsi que les différentes manières de
-                    s’impliquer dans ses projets. Les participants ont ensuite pu profiter de
-                    rencontres et échanges, tissant de nouveaux liens et trouvant parfois des
-                    partenaires pour de futurs projets. Des activités interactives, incluant jeux,
-                    quiz et discussions, ont animé la journée et renforcé les liens entre les
-                    membres. Enfin, la présentation des projets et initiatives a permis à chacun de
-                    comprendre comment contribuer concrètement et laisser sa marque dans la
-                    communauté. Cette journée a été bien plus qu’un simple événement : elle a été un
-                    véritable moment de partage, de convivialité et de découverte, apprécié autant
-                    par les nouveaux arrivants que par les membres présents depuis longtemps. La
-                    Journée Akwaba a confirmé l’engagement de la communauté à créer des liens
-                    solides et à offrir un environnement stimulant pour tous ses membres.
-                  </p>
-                </details> -->
               </div>
             </div>
           </div>
@@ -75,38 +63,19 @@
                 <u>SIDIBE GNIAMAN MAHAMADOU</u>
               </h3>
               <div class="text-gray-700 text-base">
-                Vice-président de la Communauté Virtuelle de cote d 'ivoire
-                <!-- <details>
-                  <summary>Voir Plus</summary>
-                  <p>
-                    L’événement s’est tenu le jeudi 25 septembre 2025 au sein de l’institution à
-                    Abidjan-Cocody, en présence de nombreuses autorités académiques et partenaires
-                    du monde du numérique. Une initiative pour renforcer les compétences numériques
-                    Le lancement, effectué symboliquement par M. Touré Vamara, représentant du
-                    ministre de l’Enseignement supérieur et de la Recherche scientifique, marque une
-                    nouvelle étape dans la promotion du numérique en Côte d’Ivoire. La plateforme
-                    MTN Skills Academy met à la disposition des apprenants plus de 400 ressources
-                    pédagogiques couvrant plusieurs domaines clés, notamment : le développement web,
-                    l’intelligence artificielle, la cybersécurité, et l’entrepreneuriat numérique.
-                    Ce partenariat vise à améliorer le taux d’employabilité des jeunes diplômés, à
-                    multiplier par trois le nombre de techniciens et d’ingénieurs formés au
-                    numérique et à favoriser la reconnaissance internationale des compétences
-                    acquises. Un lien fort avec le Statut national de l’étudiant entrepreneur (SNEE)
-                    Selon M. Touré Vamara, la plateforme vient renforcer le Statut national de
-                    l’étudiant entrepreneur (SNEE), un dispositif mis en place par le gouvernement
-                    pour encourager l’esprit d’initiative chez les jeunes. La combinaison du cadre
-                    légal du SNEE et des formations pratiques offertes par la Skills Academy
-                    permettra aux étudiants de transformer leurs projets en entreprises viables et
-                    d’apporter une réelle valeur ajoutée à l’économie numérique ivoirienne. Une
-                    présence remarquée des étudiants de la Communauté virtuelle de Cocody Les
-                    étudiants de la Communauté virtuelle de Cocody étaient présents en grand nombre
-                    lors de cette cérémonie. Leur participation témoigne de leur engagement actif
-                    dans la dynamique de transformation numérique initiée par l’UVCI et ses
-                    partenaires. Ils ont également pu échanger avec les responsables de la Fondation
-                    MTN-CI et de l’UVCI sur les opportunités de formation et de certification
-                    offertes par la plateforme.
-                  </p>
-                </details> -->
+                Vice-président de la Communauté Virtuelle de Cocody
+              </div>
+            </div>
+          </div>
+
+          <div class="bg-white rounded-lg shadow-md overflow-hidden">
+            <img src="/src/assets/ngues.jpg" alt="affiche" class="w-full h-75 object-cover" />
+            <div class="p-6 text-center">
+              <h3 class="text-xl font-medium text-gray-800 mb-2">
+                <u> NGUESSAN EDI DIVINE CAROLINE</u>
+              </h3>
+              <div class="text-gray-700 text-base">
+                Assistante au Président de la Communauté Virtuelle de Cocody
               </div>
             </div>
           </div>
@@ -119,30 +88,6 @@
               </h3>
               <div class="text-gray-700 text-base">
                 Chargé de la Formation de la communauté Virtuelle de Cocody
-                <!-- <details>
-                  <summary>Voir plus</summary>
-                  <p>
-                    Objectifs de la rencontre Les échanges ont porté sur la volonté du bureau de :
-                    .Servir la communauté à travers des actions concrètes et utiles aux étudiants ;
-                    -Accompagner et orienter les nouveaux membres afin de faciliter leur intégration
-                    à l’UVCI ; -Favoriser l’innovation et la formation continue dans le domaine du
-                    numérique ; -Créer un véritable réseau d’entraide et de collaboration entre les
-                    étudiants. Cette rencontre marque une étape importante dans la construction
-                    d’une communauté solidaire, dynamique et inclusive, centrée sur le partage de
-                    connaissances et l’esprit d’équipe. Déroulement de la réunion Sous la direction
-                    du président Gboho Nouffé, les membres du bureau ont échangé sur les projets
-                    prioritaires à mettre en place, notamment la création de programmes de formation
-                    interne, d’ateliers pratiques et d’activités de networking entre étudiants. Des
-                    discussions ont également porté sur l’amélioration de la communication au sein
-                    de la communauté et la valorisation des initiatives étudiantes. Résolutions et
-                    engagements À l’issue de la rencontre, les membres du bureau se sont engagés à :
-                    Promouvoir une culture de service et de leadership parmi les étudiants ; Lancer
-                    des activités formatrices et collaboratives ; Renforcer la cohésion et le
-                    sentiment d’appartenance à la communauté. Le président Gboho Nouffé a félicité
-                    les membres pour leur implication et a rappelé que « servir, former et innover »
-                    demeurent les piliers du développement de la Communauté virtuelle de Cocody.
-                  </p>
-                </details> -->
               </div>
             </div>
           </div>
@@ -153,79 +98,21 @@
               <h3 class="text-xl font-medium text-gray-800 mb-2">
                 <u>KOUASSI EDGAR</u>
               </h3>
+              <div class="text-gray-700 text-base">Chargé de la Comptabilité</div>
+            </div>
+          </div>
+
+          <div class="bg-white rounded-lg shadow-md overflow-hidden">
+            <img src="/src/assets/alex.jpg" alt="affiche" class="w-full h-75 object-cover" />
+            <div class="p-6 text-center">
+              <h3 class="text-xl font-medium text-gray-800 mb-2">
+                <u> KOFFI KOUADIO JEAN ALEX</u>
+              </h3>
               <div class="text-gray-700 text-base">
-                Chargé de la Comptabilité
-                <!-- <details>
-                  <summary>Voir plus</summary>
-                  <p>
-                    Objectifs de la rencontre Les échanges ont porté sur la volonté du bureau de :
-                    .Servir la communauté à travers des actions concrètes et utiles aux étudiants ;
-                    -Accompagner et orienter les nouveaux membres afin de faciliter leur intégration
-                    à l’UVCI ; -Favoriser l’innovation et la formation continue dans le domaine du
-                    numérique ; -Créer un véritable réseau d’entraide et de collaboration entre les
-                    étudiants. Cette rencontre marque une étape importante dans la construction
-                    d’une communauté solidaire, dynamique et inclusive, centrée sur le partage de
-                    connaissances et l’esprit d’équipe. Déroulement de la réunion Sous la direction
-                    du président Gboho Nouffé, les membres du bureau ont échangé sur les projets
-                    prioritaires à mettre en place, notamment la création de programmes de formation
-                    interne, d’ateliers pratiques et d’activités de networking entre étudiants. Des
-                    discussions ont également porté sur l’amélioration de la communication au sein
-                    de la communauté et la valorisation des initiatives étudiantes. Résolutions et
-                    engagements À l’issue de la rencontre, les membres du bureau se sont engagés à :
-                    Promouvoir une culture de service et de leadership parmi les étudiants ; Lancer
-                    des activités formatrices et collaboratives ; Renforcer la cohésion et le
-                    sentiment d’appartenance à la communauté. Le président Gboho Nouffé a félicité
-                    les membres pour leur implication et a rappelé que « servir, former et innover »
-                    demeurent les piliers du développement de la Communauté virtuelle de Cocody.
-                  </p>
-                </details> -->
+                Chargé de la communication de la Communauté Virtuelle de Cocody
               </div>
             </div>
           </div>
-          <!-- special card -->
-          <!-- <div class="bg-white rounded-lg bg-gradient-to-tr from-pink-300 to-blue-300 p-0.5 shadow-lg overflow-hidden min-h-full"
-        >
-          <div class="text-center text-white font-medium">Special product</div>
-          <img
-            src="https://images.unsplash.com/photo-1556910110-a5a63dfd393c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cmF3JTIwc3BhZ2hldHRpfGVufDB8fDB8fHww"
-            alt="Coffee"
-            class="w-full h-64 object-cover rounded-t-lg"
-          />
-          <div class="p-6 bg-white text-center rounded-b-lg md:min-h-full">
-            <h3 class="text-xl font-medium text-gray-800 mb-2">Flavoured Spaghetti</h3>
-            <p class="text-gray-700 text-base">
-              <span class="font-medium underline">Our speciality is</span>
-              <!- Bappa Flour Mill offers a variety of flavored spaghetti dishes that are sure to
-              tantalize your taste buds. We use only the freshest ingredients Our flavors include:
-              Mango, spinach -->
-          <!-- </p>
-          </div>
-        </div>
-
-        <div class="bg-white rounded-lg shadow-md overflow-hidden"> -->
-          <!-- <img
-            src="https://media.istockphoto.com/id/1265641298/photo/fried-papad.jpg?s=612x612&w=0&k=20&c=e_iEy4CTvU6Thn02zGgKt_TiSYAheCKmgfTF5j52ovU="
-            alt="papad"
-            class="w-full h-64 object-cover"
-          />
-          <div class="p-6 text-center"> -->
-          <!-- <h3 class="text-xl font-medium text-gray-800 mb-2">Rice Papad</h3>
-            <div class="text-gray-700 text-base"> -->
-          <!-- Our company produces high-quality rice papad that is made with the finest ingredients.
-              We use traditional methods to make our papad, which gives it a unique flavor and
-              texture. Our papad is also gluten-free and vegan. -->
-          <!-- <details>
-                <summary>Read More</summary>
-                <p>
-                  <!- We offer a variety of rice papad flavors, including plain, salted, spicy, and -->
-          <!-- flavored. We also offer a variety of sizes and shapes to choose from. Our papad is
-                  available in bulk or in individual packages. -->
-          <!-- </p>
-              </details>
-            </div>
-          </div>
-        </div>
-      </div>  -->
         </div>
       </div>
     </section>
